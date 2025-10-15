a45d856718aed48d276fb4ff641e49a1e338c287
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2025-10-16 01:59:25 +0300

    Pretty Tables Bug again

--- Pretty Tables bug.jl
+++ Pretty Tables bug.jl
@@ -5,3 +5,11 @@ column_labels=(["Column1","Column2","Column3"])
 data=[1 2 3]
 
 pretty_table(data;column_labels)
+
+save_folder="C:/Users/Batcomputr/Documents/GitHub/plots, mostly plots"
+
+open(joinpath(save_folder,"test-table.txt"), write=true) do io
+        pretty_table(io,data;column_labels)
+    end
+
+#oopsie
