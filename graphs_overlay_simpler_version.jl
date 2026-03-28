2039a690029951caba666551c0eddd97e13f7964
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2026-03-28 22:38:44 +0200

    Capacitance from CV

--- graphs_overlay_simpler_version.jl
+++ graphs_overlay_simpler_version.jl
@@ -7,11 +7,12 @@ using NumericalIntegration
 using ColorSchemes
 using Colors
 
-function overlay_graphs(mode,n,clr)
+function overlay_graphs(mode,n,clr,scan_rate)
 
     Figure_EIS=Figure(size=(1500,500))
     Figure_CV=Figure(size=(1000,800))
     Figure_CD=Figure(size=(1000,800))
+    Figure_CV_Cap=Figure(size=(1000,800))
 
     files_vector=[]
 
@@ -23,8 +24,24 @@ function overlay_graphs(mode,n,clr)
     xlabel="Frequency",ylabel="Z (Ω)",xscale=log10)
 
     Axis_CV=Axis(Figure_CV[1,1],title="Cyclic Voltammetry",
-    xlabel="Potential (V)",ylabel="Current (A)",xticks=LinearTicks(10),
+    xlabel="Potential (V)",ylabel="Current (mA)",xticks=LinearTicks(10),
     yticks=LinearTicks(10))
+
+    Axis_CV_Cap_Q1=Axis(Figure_CV_Cap[1,1],title="Q1 Capacitance",
+    xlabel="Cycle iteration",ylabel="Capacitance (mF)",
+    xticks=LinearTicks(9),yticks=LinearTicks(5))
+
+    Axis_CV_Cap_Q2=Axis(Figure_CV_Cap[1,2],title="Q2 Capacitance",
+    xlabel="Cycle iteration",ylabel="Capacitance (mF)",
+    xticks=LinearTicks(9),yticks=LinearTicks(5))
+
+    Axis_CV_Cap_Q3=Axis(Figure_CV_Cap[2,1],title="Q3 Capacitance",
+    xlabel="Cycle iteration",ylabel="Capacitance (mF)",
+    xticks=LinearTicks(9),yticks=LinearTicks(5))
+
+    Axis_CV_Cap_Q4=Axis(Figure_CV_Cap[2,2],title="Q4 Capacitance",
+    xlabel="Cycle iteration",ylabel="Capacitance (mF)",
+    xticks=LinearTicks(9),yticks=LinearTicks(5))
 
     Axis_C=Axis(Figure_CD[1,1],title="Charge",
     xlabel="Time (s)",ylabel="Potential (V)")
@@ -99,19 +116,50 @@ function overlay_graphs(mode,n,clr)
 
             color_i=_cmap[i]
 
-            plot_CV=lines!(Axis_CV,Potential,Current,
+            plot_CV=lines!(Axis_CV,Potential,Current*1000,
             label=basename(file),linewidth=2,color=color_i)
+
+            idx_q1=(Potential .* Current .> 0) .&& (Potential .> 0)
+            idx_q2=(Potential .* Current .< 0) .&& (Potential .> 0)
+            idx_q3=(Potential .* Current .> 0) .&& (Potential .< 0)
+            idx_q4=(Potential .* Current .< 0) .&& (Potential .< 0)
+            
+            Integral_Q1=integrate(Potential[idx_q1],Current[idx_q1])
+            Capacitance_CV_Q1=Integral_Q1/(maximum(Potential[idx_q1])*scan_rate)
+            
+            Integral_Q2=integrate(Potential[idx_q2],Current[idx_q2])
+            Capacitance_CV_Q2=Integral_Q2/(maximum(Potential[idx_q2])*scan_rate)
+            
+            Integral_Q3=integrate(Potential[idx_q3],Current[idx_q3])
+            Capacitance_CV_Q3=Integral_Q3/(maximum(abs.(Potential[idx_q3]))*scan_rate)
+
+            Integral_Q4=integrate(Potential[idx_q4],Current[idx_q4])
+            Capacitance_CV_Q4=Integral_Q4/(maximum(abs.(Potential[idx_q4]))*scan_rate)
+
+            q1=scatter!(Axis_CV_Cap_Q1,i,Capacitance_CV_Q1*1000,
+            color=color_i,markersize=20,label=basename(file))
+            q2=scatter!(Axis_CV_Cap_Q2,i,Capacitance_CV_Q2*1000,
+            color=color_i,markersize=20,label=basename(file))
+            q3=scatter!(Axis_CV_Cap_Q3,i,Capacitance_CV_Q3*1000,
+            color=color_i,markersize=20,label=basename(file))
+            q4=scatter!(Axis_CV_Cap_Q4,i,Capacitance_CV_Q4*1000,
+            color=color_i,markersize=20,label=basename(file))
+
         end
 
-        Figure_CV[2,1]=Legend(Figure_CV,Axis_CV,orientation=:horizontal,tailwidth=true)
+        Figure_CV[2,1]=Legend(Figure_CV,Axis_CV,orientation=:horizontal,tellwidth=true)
+        Figure_CV_Cap[3,1:2]=Legend(Figure_CV_Cap,Axis_CV_Cap_Q3,orientation=:horizontal)
 
         DataInspector(Figure_CV)
+        DataInspector(Figure_CV_Cap)
 
-        display(Figure_CV)
+        display(GLMakie.Screen(),Figure_CV)
+        display(GLMakie.Screen(),Figure_CV_Cap)
 
         save_folder=pick_folder()
 
         save(joinpath(save_folder,basename(save_folder)*"_CV.png"),Figure_CV)
+        save(joinpath(save_folder,basename(save_folder)*"CV_Cap.png"),Figure_CV_Cap)
 
     elseif mode == "C" || mode == "D"
 
@@ -191,7 +239,7 @@ function overlay_graphs(mode,n,clr)
     end
 end
 
-overlay_graphs("C",9,:twilight)
+overlay_graphs("CV",8,:twilight,0.1)
 
 nope .... it uses the default one only 
 you need to make a vector of colors to iterate it, I suppose 
