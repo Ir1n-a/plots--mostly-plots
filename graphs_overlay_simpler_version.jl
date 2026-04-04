58c712665742ce558eca5710097fa0ea5faf06ee
Ir1n-a <159860234+Ir1n-a@users.noreply.github.com>
2026-04-05 00:41:58 +0300

    more capacitance adjacent calculations

--- graphs_overlay_simpler_version.jl
+++ graphs_overlay_simpler_version.jl
@@ -13,6 +13,8 @@ function overlay_graphs(mode,n,clr,scan_rate)
     Figure_CV=Figure(size=(1000,800))
     Figure_CD=Figure(size=(1000,800))
     Figure_CV_Cap=Figure(size=(1000,800))
+    Figure_CV_peaks=Figure(size=(1000,800))
+    Figure_Cap_Sensi=Figure(size=(1000,800))
 
     files_vector=[]
 
@@ -55,10 +57,45 @@ function overlay_graphs(mode,n,clr,scan_rate)
     xlabel="Cycle iteration",ylabel="Capacitance (mF)",xticks=LinearTicks(9),
     yticks=LinearTicks(5))
 
-    cmap=cgrad(clr, n+3; categorical=true,rev=true)
-    _cmap=collect(cmap)
-    deleteat!(_cmap, 5)
-    deleteat!(_cmap,6)
+    cycles_vector=[1,2,3,4,5,6,7,8]
+    ΔCc=[]
+    ΔCd=[]
+
+    Cap_C=[]
+    Cap_D=[]
+
+    Axis_Cap_Sensi_dCc=Axis(Figure_Cap_Sensi[1,1],title="ΔCc vs cycle number",
+    xlabel="Number of cycles",ylabel="ΔCc (mF)",xticks=(1:8,["5000","5000","2000",
+    "5000","5000","5000","5000","3500"]),yticks=LinearTicks(5))
+    Axis_Cap_Sensi_dCd=Axis(Figure_Cap_Sensi[1,2],title="ΔCd vs cycle number",
+    xlabel="Number of cycles",ylabel="ΔCc (mF)",xticks=(1:8,["5000","5000","2000",
+    "5000","5000","5000","5000","3500"]),yticks=LinearTicks(5))
+
+    Axis_Cap_Sensi_ΔC=Axis(Figure_Cap_Sensi[2,1],title="ΔC",xlabel="Cycle iteration",
+    ylabel="ΔC (mF)",xticks=LinearTicks(9),yticks=LinearTicks(5))
+    Axis_Cap_Sensi_ϵC=Axis(Figure_Cap_Sensi[2,2],title="ϵC",xlabel="Cycle iteration",
+    ylabel="ϵC (mF)",xticks=LinearTicks(9),yticks=LinearTicks(5))
+
+
+    Axis_CV_peaks_positive_V=Axis(Figure_CV_peaks[1,1],title="Positive voltage peak around 0V",
+    xlabel="Cycle iteration",ylabel="Potential value (V)",
+    xticks=LinearTicks(9),yticks=LinearTicks(5))
+    Axis_CV_peaks_negative_V=Axis(Figure_CV_peaks[1,2],title="Negative voltage peak around 0V ",
+    xlabel="Cycle iteration",ylabel="Potential value (V)")
+
+    if n==9
+
+        cmap=cgrad(clr, n+3; categorical=true,rev=true)
+        _cmap=collect(cmap)
+        deleteat!(_cmap, 5)
+        deleteat!(_cmap,6)
+
+    else n=8
+        cmap=cgrad(clr,n+3;categorical=true,rev=true)
+        _cmap=collect(cmap)
+        deleteat!(_cmap,5)
+        deleteat!(_cmap,9)
+    end
 
 
     println("pick the files you want, preferably of the same measurement")
@@ -191,10 +228,13 @@ function overlay_graphs(mode,n,clr,scan_rate)
             Capacitance_Charging=(2*abs(Average_Charging_Current)*Integral_Charging_Potential)/
             (Maximum_Charging_Potential^2)
 
+            push!(Cap_C,Capacitance_Charging)
+
             plot_C_Cap=scatter!(Axis_C_Cap,i,Capacitance_Charging*1000
             ,color=color_i,markersize=20)
         end
 
+        
         println("pick the discharging files")
 
         for j in 1:n
@@ -222,24 +262,51 @@ function overlay_graphs(mode,n,clr,scan_rate)
             plot_D_Cap=scatter!(Axis_D_Cap,j,Capacitance_Discharging*1000,
             color=color_j,markersize=20)
 
+            push!(Cap_D,Capacitance_Discharging)
+
         end
 
         Figure_CD[3,1:2]=Legend(Figure_CD,Axis_C,orientation=:horizontal)
 
         DataInspector(Figure_CD)
 
-        display(Figure_CD)
+        display(GLMakie.Screen(),Figure_CD)
 
         save_folder=pick_folder()
 
         save(joinpath(save_folder,basename(save_folder)*"_CD.png"),Figure_CD)
+
+        for i in 2:n
+            push!(ΔCc,abs(Cap_C[i]-Cap_C[i-1]))
+            push!(ΔCd,abs(Cap_D[i]-Cap_D[i-1]))
+
+        end
+        
+        cycles_i=[1,2,3,4,5,6,7,8,9]
+
+        plot_ΔCc=scatter!(Axis_Cap_Sensi_dCc,ΔCc*1000,
+        markersize=20)
+        plot_ΔCd=scatter!(Axis_Cap_Sensi_dCd,ΔCd*1000,
+        markersize=20)
+        plot_ΔC=scatter!(Axis_Cap_Sensi_ΔC,cycles_i,abs.(Cap_C .- Cap_D)*1000,
+        markersize=20)
+        plot_ϵC=scatter!(Axis_Cap_Sensi_ϵC,cycles_i,abs.(Cap_C ./ Cap_D),
+        markersize=20)
+        
+        DataInspector(Figure_Cap_Sensi)
+
+        display(GLMakie.Screen(),Figure_Cap_Sensi)
+
+        save_folder=pick_folder()
+
+        save(joinpath(save_folder,basename(save_folder)*"_Cc.png"),Figure_Cap_Sensi)
 
 
 
     end
 end
 
-overlay_graphs("CV",8,:twilight,0.1)
+overlay_graphs("C",9,:twilight,0.5)
 
 nope .... it uses the default one only 
 you need to make a vector of colors to iterate it, I suppose 
